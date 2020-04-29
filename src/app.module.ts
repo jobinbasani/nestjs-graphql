ce4ddbd7d57cc50871799cd09955634fb90a4645
Jobin Basani <jobinbasani@gmail.com>
2020-04-29 11:23:39 -0300

    add entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
+import { Lesson } from './lesson/lesson.entity';
 import { Module } from '@nestjs/common';
 import { GraphQLModule} from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type:'mongodb',
+      url:'mongodb://localhost/school',
+      synchronize:true,
+      useUnifiedTopology:true,
+      entities:[
+        Lesson
+      ]
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true
     }),
--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,7 +1,13 @@
+import { Lesson } from './lesson.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { LessonResolver } from './lesson.resolver';
+import { LessonService } from './lesson.service';
 
 @Module({
-    providers:[LessonResolver]
+    imports:[
+        TypeOrmModule.forFeature([Lesson])
+    ],
+    providers:[LessonResolver, LessonService]
 })
 export class LessonModule {}
